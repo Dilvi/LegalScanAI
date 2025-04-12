d7e07ff9824b03e6ba59c7b6a68b47b35cb7a598
Dilvi <artem04032004@yandex.ru>
2025-04-12 22:38:36 +0300

    Add share_function

--- backend_services/anonymizer.py
+++ backend_services/anonymizer.py
@@ -1,16 +1,20 @@
+import re
+
 class Anonymizer:
     def __init__(self):
         pass
 
     def anonymize(self, text: str, entities: list):
         """
-        Заменяет личные данные в тексте на метки.
+        Заменяет личные данные в тексте на уникальные метки (например: [[PER_1]], [[ADDRESS_2]]).
         """
         anonymized_text = text
+        # Сортировка по длине текста, чтобы избежать перекрытий
         entities = sorted(entities, key=lambda x: len(x['text']), reverse=True)
 
         for entity in entities:
-            placeholder = f"[[{entity['type']}]]"
-            anonymized_text = anonymized_text.replace(entity['text'], placeholder)
+            placeholder = f"[[{entity['label']}]]"
+            # Безопасная замена (например, чтобы не заменить одно и то же несколько раз)
+            anonymized_text = re.sub(re.escape(entity['text']), placeholder, anonymized_text)
 
         return anonymized_text
